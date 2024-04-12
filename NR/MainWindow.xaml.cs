9d6ed1add4cf3844ed51523b8e5eeed8a7ad72f5
RetyunskikhAS <RetyunakikhAS@quarta.su>
2024-04-12 17:17:58 +0300

    init

---
+++ NR/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace NR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyInkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+            MyInkCanvas.DefaultDrawingAttributes.Width = 1;
+            MyInkCanvas.DefaultDrawingAttributes.Height = 1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var strokes = MyInkCanvas.Strokes;
+        }
+    }
+}
